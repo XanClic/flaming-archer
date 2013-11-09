1988e59577c76b44df628a16d69402cc5fb09eb5
Max Reitz <xanclic@gmail.com>
2013-11-09 17:57:19 +0100

    Partikel: Einfacher gestaltet
    * Kräfte auf Bitmaske reduziert
    * In den dake-Namespace ausgelagert
    * Lifetime und Kräfte basieren jetzt auf der vergangenen Zeit seit dem
      letzten Aufruf

--- dake/particles.h
+++ dake/particles.h
@@ -3,28 +3,38 @@
 
 #include <list>
 
-#include "obj_reader.h"
+#include <cgv/math/fvec.h>
+
+typedef cgv::math::fvec<float, 3> vec3;
 
 
-typedef vec3 (*force_apply_t)(const vec3 &position, const vec3 &speed);
+namespace dake
+{
 
-extern vec3 force_air_drag(const vec3 &position, const vec3 &speed);
-extern vec3 force_gravity(const vec3 &position, const vec3 &speed);
-extern vec3 force_bounce(const vec3 &position, const vec3 &speed);
+extern vec3 force_air_drag(const vec3 &position, const vec3 &speed, float time_passed);
+extern vec3 force_gravity (const vec3 &position, const vec3 &speed, float time_passed);
+extern vec3 force_bounce  (const vec3 &position, const vec3 &speed, float time_passed);
 
 
 class particle
 {
     private:
         vec3 position, speed;
-        std::list<void *> forces;
-        int lifetime_rem, lifetime_full;
+        float lifetime_rem, lifetime_full;
+        int force_mask;
 
     public:
-        particle(int lifetime, const vec3 &initial_position, const vec3 &initial_speed);
+        enum force
+        {
+            AIR_DRAG,
+            GRAVITY,
+            BOUNCE
+        };
 
-        particle &add_force(force_apply_t force);
-        void apply_forces(void);
+        particle(float lifetime, const vec3 &initial_position, const vec3 &initial_speed);
+
+        particle &add_force(force f);
+        void apply_forces(float time_passed);
         void draw(void);
         float lifetime(void);
 };
@@ -37,7 +47,7 @@ class particle_generator
 
     public:
         particle &new_particle(int lifetime, const vec3 &initial_position, const vec3 &initial_speed);
-        void tick(void);
+        void tick(float time_passed);
         void draw(void);
 
         static particle_generator &instance(void)
@@ -47,5 +57,7 @@ class particle_generator
             return *pgen;
         }
 };
+
+}
 
 #endif
